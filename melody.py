c28d47e76403f683ffc9280535d552314c68ec82
zacharylyj <116093401+zacharylyj@users.noreply.github.com>
2022-11-15 23:29:11 +0800

    Update melody.py

--- melody.py
+++ melody.py
@@ -1,10 +1,8 @@
 from pyautogui import *
-from multiprocessing import Process
 import pyautogui
 import time
 import keyboard
-import win32api, win32con
-import os   
+import win32api, win32con  
        
 def lclick(x,y):
     win32api.SetCursorPos((x,y))
@@ -13,43 +11,51 @@ def lclick(x,y):
     time.sleep(0.05)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)      
 
-time.sleep(2.5)
+time.sleep(3)
 print("now")
 i = 0
-while(not(keyboard.is_pressed('ctrl'))):   
-    if (pyautogui.pixel(777, 457)[0] == 56):
-        #time.sleep(0.1)
-        lclick(777,497)
-        time.sleep(0.05)
-        win32api.SetCursorPos((60,500))
-    if (pyautogui.pixel(832, 457)[0] == 56):
-        #time.sleep(0.1)
-        lclick(832,497)
-        time.sleep(0.05)
-        win32api.SetCursorPos((60,500))
-    if (pyautogui.pixel(886, 457)[0] == 56):
-        #time.sleep(0.1)
-        lclick(886,497)
-        time.sleep(0.05)
-        win32api.SetCursorPos((60,500))
-    if (pyautogui.pixel(940, 457)[0] == 56):
-        #time.sleep(0.1)
-        lclick(940,497)
-        time.sleep(0.05)
-        win32api.SetCursorPos((60,500))
-    if (pyautogui.pixel(993, 457)[0] == 56):
-        #time.sleep(0.1)
-        lclick(993,497)
-        time.sleep(0.05)
-        win32api.SetCursorPos((60,500))
-    if (pyautogui.pixel(1047, 457)[0] == 56):
-        #time.sleep(0.1)
-        lclick(1047,497)
-        time.sleep(0.05)
-        win32api.SetCursorPos((60,500))
-    if (pyautogui.pixel(1100, 457)[0] == 56):
-        #time.sleep(0.1)
-        lclick(1100,497)
-        time.sleep(0.05)
-        win32api.SetCursorPos((60,500))
+while(not(keyboard.is_pressed('ctrl'))):
+    if(keyboard.is_pressed('[')):
+        i = 0
+        print("start")
+    while(i == 0):   
+        if(keyboard.is_pressed(']')):
+            i = 1
+            print("stop")
+
+        if (pyautogui.pixel(777, 417)[0] == 56):
+            #time.sleep(0.1)
+            lclick(777,497)
+            time.sleep(0.05)
+            win32api.SetCursorPos((60,500))
+        if (pyautogui.pixel(832, 417)[0] == 56):
+            #time.sleep(0.1)
+            lclick(832,497)
+            time.sleep(0.05)
+            win32api.SetCursorPos((60,500))
+        if (pyautogui.pixel(886, 417)[0] == 56):
+            #time.sleep(0.1)
+            lclick(886,497)
+            time.sleep(0.05)
+            win32api.SetCursorPos((60,500))
+        if (pyautogui.pixel(940, 417)[0] == 56):
+            #time.sleep(0.1)
+            lclick(940,497)
+            time.sleep(0.05)
+            win32api.SetCursorPos((60,500))
+        if (pyautogui.pixel(993, 417)[0] == 56):
+            #time.sleep(0.1)
+            lclick(993,497)
+            time.sleep(0.05)
+            win32api.SetCursorPos((60,500))
+        if (pyautogui.pixel(1047, 417)[0] == 56):
+            #time.sleep(0.1)
+            lclick(1047,497)
+            time.sleep(0.05)
+            win32api.SetCursorPos((60,500))
+        if (pyautogui.pixel(1100, 417)[0] == 56):
+            #time.sleep(0.1)
+            lclick(1100,497)
+            time.sleep(0.05)
+            win32api.SetCursorPos((60,500))
 
